0b83787c025c5f07ab8d37e5665fc7d67ceb13a6
Shahraaz Hussain <shahraazhussain@gmail.com>
2019-06-19 00:39:47 +0530

    Bash_Auto_Commit

--- Commit.sh
+++ Commit.sh
@@ -6,6 +6,9 @@ then
 else
 	commit_message=$1
 fi
+dt=$(date '+%d/%m/%Y %H:%M:%S');
+commit_message= "$commit_message dt"
+
 direct='0'
 if [ -z "$2" ]
 then
@@ -20,6 +23,7 @@ then
 	git commit -m $commit_message
 	git push -u origin master
 fi
+
 folder=$(ls $2)
 for f in $folder
 do	if [ -d $f ]
