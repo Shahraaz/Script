ae90e8427e5e570e4dc6dacab4939057f4f05189
Shahraaz Hussain <shahraazhussain@gmail.com>
2019-07-12 03:30:35 +0530

    Fri, Jul 12 03:30 globalCommit

--- Commit.sh
+++ Commit.sh
@@ -2,40 +2,40 @@
 commit_message='0'
 if [ -z "$1" ]
 then
-	commit_message='Bash_Auto_Commit'
+    commit_message='Bash_Auto_Commit'
 else
-	commit_message=$1
+    commit_message=$1
 fi
 echo $commit_message
 direct='0'
 if [ -z "$2" ]
 then
-	direct=$(pwd)
+    direct=$(pwd)
 else
-	direct=$2
+    direct=$2
 fi
 cd $direct
 git add --all
 if [ $? -eq 0 ]
 then
-	git commit -m $commit_message
-	git push -u origin master
+    git commit -m "$(date +"%a, %b %d %H:%M") $commit_message"
+    git push -u origin master
+else
+    folder=$(ls $2)
+    for f in $folder
+    do	if [ -d $f ]
+        then
+            cd $f
+            ls .git
+            if [ $? -eq 1 ]
+            then
+                cd ..
+            else
+                git add --all
+                git commit -m "$(date +"%a, %b %d %H:%M") $commit_message"
+                git push -u origin master
+                cd ..
+            fi
+        fi
+    done
 fi
-
-folder=$(ls $2)
-for f in $folder
-do	if [ -d $f ]
-	then
-		cd $f
-		ls .git
-		if [ $? -eq 1 ]
-		then
-			cd ..
-		else
-			git add --all
-			git commit -m $commit_message
-			git push -u origin master
-			cd ..
-		fi
-	fi
-done
