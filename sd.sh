3d103471b5be24f7e342381d86f30c2f0af33078
Shahraaz Hussain <shahraazhussain@gmail.com>
2019-07-14 23:17:46 +0530

    Sun, Jul 14 23:17 BeforeShutdown

--- sd.sh
+++ sd.sh
@@ -10,10 +10,12 @@ if [ -z "$2" ]
 then
 	wget --tries=1 http://172.16.12.1:1000/logout?/
 	wget --tries=1 http://192.168.41.1:1000/logout?/
+	wget --tries=1 http://192.168.5.1:1000/logout?/
 	rm logout@%2F
 	shutdown -s -t 0
 else
 	wget --tries=1 http://172.16.12.1:1000/logout?/
 	wget --tries=1 http://192.168.41.1:1000/logout?/
+	wget --tries=1 http://192.168.5.1:1000/logout?/
 	rm logout@%2F
 fi
