5299a5eb1d49085eb4c64c32561c244e5791cf06
Shahraaz Hussain <shahraazhussain@gmail.com>
2019-07-15 17:53:30 +0530

    Mon, Jul 15 17:53 BeforeShutdown

--- sd.sh
+++ sd.sh
@@ -8,13 +8,13 @@ fi
 CommitGlobal.sh $commit_message
 if [ -z "$2" ]
 then
-	wget --tries=1 http://172.16.12.1:1000/logout?/
+	wget --tries=1 http://172.16.24.1:1000/logout?/
 	wget --tries=1 http://192.168.41.1:1000/logout?/
 	wget --tries=1 http://192.168.5.1:1000/logout?/
 	rm logout@%2F
 	shutdown -s -t 0
 else
-	wget --tries=1 http://172.16.12.1:1000/logout?/
+	wget --tries=1 http://172.16.24.1:1000/logout?/
 	wget --tries=1 http://192.168.41.1:1000/logout?/
 	wget --tries=1 http://192.168.5.1:1000/logout?/
 	rm logout@%2F
