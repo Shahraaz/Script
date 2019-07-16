5b73dd58944411b8b426ba5a5d4e4f2365b86174
Shahraaz Hussain <shahraazhussain@gmail.com>
2019-07-16 21:42:22 +0530

    Tue, Jul 16 21:42 Automatic Logger [

---
+++ login.py
@@ -0,0 +1,60 @@
+import requests
+import sys
+import os
+from time import sleep
+
+USER = input("username: ")
+PASS = input("password: ")
+
+
+def login():
+    url = "http://detectportal.firefox.com/status.txt"
+    resp = requests.get(url)
+    if resp.url == url:
+        print("Already logged in")
+        return None, None
+    server_url = resp.url
+    i = server_url.find("fgtauth")
+    server_ip = server_url[0:i]
+    print("Logged in to Server: ", server_ip)
+
+    login_page = requests.get(server_url).text
+    start_text = "name=\"magic\" value=\""
+    i = login_page.find(start_text) + len(start_text)
+    j = login_page.find("\"", i)
+    magic = login_page[i:j]
+
+    resp = requests.post(server_ip, data={
+        "magic": magic,
+        "username": USER,
+        "password": PASS
+    })
+    i = resp.text.find(server_ip+"keepalive")
+    j = resp.text.find("\"", i)
+    keepalive_url = resp.text[i:j]
+    return server_ip, keepalive_url
+
+
+def keepalive(keepalive_url):
+    print("Keepalive...")
+    if keepalive_url == None:
+        return
+    requests.get(keepalive_url)
+
+
+def logout(server_ip):
+    print(server_ip)
+    if server_ip == None:
+        return
+    print("Bbye")
+    os.system("commitGlobal.sh")
+    requests.get(server_ip+"logout?a")
+
+
+try:
+    server_url, keepalive_url = login()
+    while True:
+        sleep(2000)
+        keepalive(keepalive_url)
+except KeyboardInterrupt:
+    logout(server_url)
--- sd.sh
+++
@@ -1,7 +0,0 @@
-IFS= read -r -p "Enter Commit Message: " commit_message
-CommitGlobal.sh "$commit_message"
-wget --tries=1 http://172.16.24.1:1000/logout?/
-wget --tries=1 http://192.168.41.1:1000/logout?/
-wget --tries=1 http://192.168.5.1:1000/logout?/
-rm logout@%2F
-shutdown -s -t 0
