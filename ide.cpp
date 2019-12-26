9a2aede0ba96bb909c95cbdbe4b407b6bb92f253
Shahraaz Hussain <ShahraazHussain@gmail.com>
2019-12-26 23:43:05 +0530

    Thu 26 Dec 2019 11:43:05 PM IST AutoCommit

--- ide.cpp
+++ ide.cpp
@@ -1,36 +1,53 @@
 //Optimise
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 using namespace std;
+using namespace __gnu_pbds;
 
-// #define multitest 1
-#ifdef WIN32
+// #define MULTI_TEST
+#ifdef LOCAL
 #define db(...) ZZ(#__VA_ARGS__, __VA_ARGS__);
 #define pc(...) PC(#__VA_ARGS__, __VA_ARGS__);
 template <typename T, typename U>
 ostream &operator<<(ostream &out, const pair<T, U> &p)
 {
-	out << '[' << p.first << ", " << p.second << ']';
-	return out;
+    out << '[' << p.first << ", " << p.second << ']';
+    return out;
 }
 template <typename Arg>
 void PC(const char *name, Arg &&arg)
 {
-	std::cerr << name << " { ";
-	for (const auto &v : arg)
-		cerr << v << ' ';
-	cerr << " }\n";
+    while (*name == ',' || *name == ' ')
+        name++;
+    std::cerr << name << " { ";
+    for (const auto &v : arg)
+        cerr << v << ' ';
+    cerr << " }\n";
+}
+template <typename Arg1, typename... Args>
+void PC(const char *names, Arg1 &&arg1, Args &&... args)
+{
+    while (*names == ',' || *names == ' ')
+        names++;
+    const char *comma = strchr(names, ',');
+    std::cerr.write(names, comma - names) << " { ";
+    for (const auto &v : arg1)
+        cerr << v << ' ';
+    cerr << " }\n";
+    PC(comma, args...);
 }
 template <typename Arg1>
 void ZZ(const char *name, Arg1 &&arg1)
 {
-	std::cerr << name << " = " << arg1 << endl;
+    std::cerr << name << " = " << arg1 << endl;
 }
 template <typename Arg1, typename... Args>
 void ZZ(const char *names, Arg1 &&arg1, Args &&... args)
 {
-	const char *comma = strchr(names + 1, ',');
-	std::cerr.write(names, comma - names) << " = " << arg1;
-	ZZ(comma, args...);
+    const char *comma = strchr(names + 1, ',');
+    std::cerr.write(names, comma - names) << " = " << arg1;
+    ZZ(comma, args...);
 }
 #else
 #define db(...)
@@ -38,38 +55,41 @@ void ZZ(const char *names, Arg1 &&arg1, Args &&... args)
 #endif
 
 using ll = long long;
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 #define f first
 #define s second
 #define pb push_back
-const long long mod = 1000000007;
+#define all(v) v.begin(),v.end()
 auto TimeStart = chrono::steady_clock::now();
+auto seed = TimeStart.time_since_epoch().count();
+std::mt19937 rng(seed);
+template <typename T>
+using Random = std::uniform_int_distribution<T>;
 
-const int nax = 2e5 + 10;
+const int NAX = 2e5 + 5, MOD = 1000000007;
 
-void solve()
+void solveCase(int caseNo)
 {
-	srand(time(NULL));
-	int x;
-	while (true)
-	{
-		cout << rand() % 10 << '\n';
-		flush(cout);
-		cin >> x;
-	}
 }
 
 int main()
 {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
-	int t = 1;
-#ifdef multitest
-	cin >> t;
+#ifndef LOCAL
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
 #endif
-	while (t--)
-		solve();
-#ifdef WIN32
-	cerr << "\n\nTime elapsed: " << chrono::duration<double>(chrono::steady_clock::now() - TimeStart).count() << " seconds.\n";
+    int t = 1;
+#ifdef MULTI_TEST
+    cin >> t;
 #endif
-	return 0;
+    for (int i = 1; i <= t; ++i)
+    {
+        solveCase(i);
+#ifdef TIME
+        cerr << "Case #" << i << ": Time " << chrono::duration<double>(chrono::steady_clock::now() - TimeStart).count() << " s.\n";
+        TimeStart = chrono::steady_clock::now();
+#endif
+    }
+    return 0;
 }
