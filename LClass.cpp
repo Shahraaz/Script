0fa6dd455915734b047e166a816061b1fb5586ed
Shahraaz Hussain <ShahraazHussain@gmail.com>
2020-01-11 12:44:59 +0530

    Sat 11 Jan 2020 12:44:59 PM IST AutoCommit

--- LClass.cpp
+++ LClass.cpp
@@ -1,7 +1,12 @@
+// Optimise
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 using namespace std;
-#define Debug
-#ifdef Debug
+using namespace __gnu_pbds;
+
+// #define MULTI_TEST
+#ifdef LOCAL
 #define db(...) ZZ(#__VA_ARGS__, __VA_ARGS__);
 #define pc(...) PC(#__VA_ARGS__, __VA_ARGS__);
 template <typename T, typename U>
@@ -13,10 +18,24 @@ ostream &operator<<(ostream &out, const pair<T, U> &p)
 template <typename Arg>
 void PC(const char *name, Arg &&arg)
 {
+    while (*name == ',' || *name == ' ')
+        name++;
     std::cerr << name << " { ";
     for (const auto &v : arg)
         cerr << v << ' ';
     cerr << " }\n";
+}
+template <typename Arg1, typename... Args>
+void PC(const char *names, Arg1 &&arg1, Args &&... args)
+{
+    while (*names == ',' || *names == ' ')
+        names++;
+    const char *comma = strchr(names, ',');
+    std::cerr.write(names, comma - names) << " { ";
+    for (const auto &v : arg1)
+        cerr << v << ' ';
+    cerr << " }\n";
+    PC(comma, args...);
 }
 template <typename Arg1>
 void ZZ(const char *name, Arg1 &&arg1)
@@ -36,12 +55,19 @@ void ZZ(const char *names, Arg1 &&arg1, Args &&... args)
 #endif
 
 using ll = long long;
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 #define f first
 #define s second
 #define pb push_back
-const long long mod = 1000000007;
+#define all(v) v.begin(), v.end()
+auto TimeStart = chrono::steady_clock::now();
+auto seed = TimeStart.time_since_epoch().count();
+std::mt19937 rng(seed);
+template <typename T>
+using Random = std::uniform_int_distribution<T>;
 
-const int nax = 2e5 + 10;
+const int NAX = 2e5 + 5, MOD = 1000000007;
 
 class Solution
 {
@@ -54,6 +80,21 @@ public:
 #ifdef LOCAL
 int main()
 {
-    Solution s;
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int t = 1;
+#ifdef MULTI_TEST
+    cin >> t;
+#endif
+    Solution S;
+    for (int i = 1; i <= t; ++i)
+    {
+        ;
+#ifdef TIME
+        cerr << "Case #" << i << ": Time " << chrono::duration<double>(chrono::steady_clock::now() - TimeStart).count() << " s.\n";
+        TimeStart = chrono::steady_clock::now();
+#endif
+    }
+    return 0;
 }
 #endif
