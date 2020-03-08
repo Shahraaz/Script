5cec472d7d7137881fde91fa5a02587f1aa8cafc
Mohammed Shahraaz Hussain <shahraazhussain@gmail.com>
2020-03-08 20:33:00 +0530

    Sunday 08 March 2020 08:33:00 PM IST AutoCommit

--- 0stemp.cpp
+++ 0stemp.cpp
@@ -7,48 +7,7 @@ using namespace __gnu_pbds;
 
 // #define MULTI_TEST
 #ifdef LOCAL
-#define db(...) ZZ(#__VA_ARGS__, __VA_ARGS__);
-#define pc(...) PC(#__VA_ARGS__, __VA_ARGS__);
-template <typename T, typename U>
-ostream &operator<<(ostream &out, const pair<T, U> &p)
-{
-    out << '[' << p.first << ", " << p.second << ']';
-    return out;
-}
-template <typename Arg>
-void PC(const char *name, Arg &&arg)
-{
-    while (*name == ',' || *name == ' ')
-        name++;
-    std::cerr << name << " { ";
-    for (const auto &v : arg)
-        cerr << v << ' ';
-    cerr << " }\n";
-}
-template <typename Arg1, typename... Args>
-void PC(const char *names, Arg1 &&arg1, Args &&... args)
-{
-    while (*names == ',' || *names == ' ')
-        names++;
-    const char *comma = strchr(names, ',');
-    std::cerr.write(names, comma - names) << " { ";
-    for (const auto &v : arg1)
-        cerr << v << ' ';
-    cerr << " }\n";
-    PC(comma, args...);
-}
-template <typename Arg1>
-void ZZ(const char *name, Arg1 &&arg1)
-{
-    std::cerr << name << " = " << arg1 << endl;
-}
-template <typename Arg1, typename... Args>
-void ZZ(const char *names, Arg1 &&arg1, Args &&... args)
-{
-    const char *comma = strchr(names + 1, ',');
-    std::cerr.write(names, comma - names) << " = " << arg1;
-    ZZ(comma, args...);
-}
+#include "/home/shahraaz/bin/debug.h"
 #else
 #define db(...)
 #define pc(...)
