169cb2af4ec37862e1de0ae15bf1dfc262bd76ad
Mohammed Shahraaz Hussain <shahraazhussain@gmail.com>
2020-03-27 22:39:03 +0530

    Friday 27 March 2020 10:39:03 PM IST AutoCommit

--- ide.cpp
+++ ide.cpp
@@ -31,88 +31,11 @@ const int NAX = 2e5 + 5, MOD = 1000000007;
 class Solution
 {
 private:
-    map<pair<int, pair<int, int>>, int> dp;
-    int grundy(int x, int y, int z)
-    {
-        if (dp.count({x, {y, z}}))
-            return dp[{x, {y, z}}];
-        int ret = 0;
-        if (x == 0)
-        {
-            if (y == 0)
-            {
-                ret = z & 1;
-            }
-            else
-            {
-                ret = z & 1;
-            }
-            db(x, y, z, ret);
-            return dp[{x, {y, z}}] = ret;
-        }
-        set<int> temp;
-        if (x > 0)
-            temp.insert(grundy(x - 1, y + 1, z));
-        if (y > 0)
-            temp.insert(grundy(x, y - 1, z + 1));
-        if (z > 0)
-            temp.insert(grundy(x, y, z - 1));
-        while (temp.count(ret))
-        {
-            ret++;
-        }
-        db(x, y, z, ret);
-        return dp[{x, {y, z}}] = ret;
-    }
-
 public:
     Solution() {}
     ~Solution() {}
     void solveCase()
     {
-        grundy(3, 3, 3);
-        int n;
-        cin >> n;
-        vector<int> p(n);
-        for (auto &x : p)
-            cin >> x;
-        vector<int> a;
-        for (int i = 0; i < n; i++)
-        {
-            if (i == 0)
-            {
-                a.pb(p[0] - 1);
-            }
-            else
-                a.pb(p[i] - p[i - 1] - 1);
-        }
-        pc(a);
-        reverse(all(a));
-        while (a.size() && a.back() == 0)
-            a.pop_back();
-        reverse(all(a));
-        pc(a);
-        if (a.size() == 0)
-        {
-            cout << "Johnny wins\n";
-        }
-        else
-        {
-            if (a.size() % 2 == 0)
-            {
-                if (a.back() % 2 == 0)
-                    cout << "Johnny wins\n";
-                else
-                    cout << "Mary wins\n";
-            }
-            else
-            {
-                if (a.back() & 1)
-                    cout << "Johnny wins\n";
-                else
-                    cout << "Mary wins\n";
-            }
-        }
     }
 };
 
