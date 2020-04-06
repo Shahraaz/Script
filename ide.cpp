9b95fe5dbbd945bdb9c99ee90df356ba5418608f
Mohammed Shahraaz Hussain <shahraazhussain@gmail.com>
2020-04-06 13:15:09 +0530

    Monday 06 April 2020 01:15:09 PM IST AutoCommit

--- ide.cpp
+++ ide.cpp
@@ -1,88 +1,125 @@
-// Optimise
 #include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
 using namespace std;
-using namespace __gnu_pbds;
 
-#define MULTI_TEST
-#ifdef LOCAL
-#include "/home/shahraaz/bin/debug.h"
-#else
-#define db(...)
-#define pc(...)
-#endif
+const int N = 100000 + 50;
 
-#define f first
-#define s second
-#define pb push_back
-#define all(v) v.begin(), v.end()
-auto TimeStart = chrono::steady_clock::now();
-auto seed = TimeStart.time_since_epoch().count();
-std::mt19937 rng(seed);
-using ll = long long;
-template <typename T>
-using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
-template <typename T>
-using Random = std::uniform_int_distribution<T>;
+vector<int> pos[5000 + 50];
 
-const int NAX = 2e5 + 5, MOD = 1000000007;
+int a[5000 + 50];
 
-class Solution
+int dp[5000 + 50][5000 + 50];
+bool vis[5000 + 50][5000 + 50];
+
+int xorr[5000 + 50][5000 + 50];
+
+int solve(int le, int ri)
 {
-private:
-public:
-    Solution() {}
-    ~Solution() {}
-    void solveCase()
+    if (le > ri)
     {
-        string str;
-        cin >> str;
-        int currDepth = 0;
-        for (auto &c : str)
+        return 0;
+    }
+    if (vis[le][ri] == true)
+    {
+        return dp[le][ri];
+    }
+    int ans = solve(le + 1, ri);
+    if (le == pos[a[le]][0])
+        for (int i = le; i <= ri; i++)
         {
-            int x = c - '0';
-            // cout << x;
-            while (currDepth > x)
+            int cur = a[i];
+            if (le > pos[a[i]][0])
+                break;
+            if (i == pos[cur][0])
             {
-                cout << ')';
-                currDepth--;
+
+                int last = pos[cur].size();
+                last--;
+                if (pos[cur][last] <= ri)
+                {
+                    // cout<<le<<" : "<<ri<<" --> ";
+                    // cout<<xorr[pos[cur][0]][pos[cur][last]]+solve(pos[cur][last]+1,ri)<<"\n";
+                    ans = max(ans, xorr[pos[cur][0]][pos[cur][last]] + solve(pos[cur][last] + 1, ri));
+                }
             }
-            while (currDepth < x)
-            {
-                cout << '(';
-                currDepth++;
-            }
-            cout << x;
-        }
-        while (currDepth--)
-        {
-            cout << ')';
         }
 
-        cout << '\n';
-    }
-};
+    // cout << le << " -> " << ri << " ,, " << ans << "\n";
+    dp[le][ri] = ans;
+    vis[le][ri] = true;
+    return dp[le][ri];
+}
 
-int32_t main()
+void fill(int n)
 {
-#ifndef LOCAL
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-#endif
-    int t = 1;
-#ifdef MULTI_TEST
-    cin >> t;
-#endif
-    Solution mySolver;
-    for (int i = 1; i <= t; ++i)
+    for (int j = 0; j <= 5000; j++)
     {
-        cout << "Case #" << i << ": ";
-        mySolver.solveCase();
-#ifdef TIME
-        cerr << "Case #" << i << ": Time " << chrono::duration<double>(chrono::steady_clock::now() - TimeStart).count() << " s.\n";
-        TimeStart = chrono::steady_clock::now();
-#endif
+        if (pos[j].size() > 0)
+        {
+
+            int le = pos[j][0];
+            int ri = pos[j][pos[j].size() - 1];
+            int val = 0;
+            int cc = 0;
+            for (int i = le; i <= ri; i++)
+            {
+                int cur = a[i];
+                int lee = pos[cur][0];
+                int rii = pos[cur][pos[cur].size() - 1];
+
+                if (i == lee)
+                {
+                    cc++;
+                }
+                if (i == rii)
+                {
+                    cc--;
+                    val ^= a[i];
+                }
+                // cout<<val<<",";
+            }
+            if (cc == 0)
+            {
+                // cout<<j<<" : "<<val<<"\n";
+                xorr[le][ri] = val;
+            }
+        }
     }
+
+    // for(int i=1;i<=n;i++){
+    //     for(int j=i;j<=n;j++){
+    //         cout<<xorr[i][j]<<" ";
+    //     }
+    //     cout<<"\n";
+    // }
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> a[i];
+        pos[a[i]].push_back(i);
+    }
+    fill(n);
+    vector<pair<int, int>> bc;
+    // for(int i=1;i<=5000;i++){
+    //     if(pos[i].size()>0){
+    //         bc.push_back({pos[i][0],pos[i][pos[i].size()-1]});
+    //     }
+    // }
+    // sort(bc.begin(),bc.end());
+    cout << solve(1, n);
     return 0;
 }
+/*
+Mistakes to take care of-
+1. For string s, s+=a is similar to appending to s. s = s + a takes O|s| + O|a| time. 
+Relevant problem where I made mistake - https://codeforces.com/contest/1326/problem/D2
+2. Try using constant value of N to declare arrays, etc. Else you might get hacked / FST.
+Relevant problem where I made a mistake - https://codeforces.com/contest/1307/problem/D
+3. While exponentiation, avoid modding the exponent. Or mod with mod-1.
+Relevant problem where I made a mistake - https://codeforces.com/contest/1332/problem/E
+*/
